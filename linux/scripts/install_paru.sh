30b2d9065b9d8d930297d26289dc28a4b1c6c0c6
aldenluthfi <aldenluthfi@gmail.com>
2024-05-01 10:34:46 +0700

    Updated

--- linux/scripts/install_paru.sh
+++ linux/scripts/install_paru.sh
@@ -1,3 +1,12 @@
+start = $(pwd)
+
+cd $(dirname $0)
+
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+
+cd ..
+rm -rf paru
+
+cd $start
--- linux/scripts/unstow_dotfiles.sh
+++ linux/scripts/unstow_dotfiles.sh
@@ -1,6 +1,6 @@
 start = $(pwd)
 
 cd $(dirname $0)/../dotfiles
-stow */ --target=$HOME
+stow */ -D --target=$HOME
 
 cd $start
