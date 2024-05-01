4f1e524256d691231e6d866a31c0e081a36e8cfa
aldenluthfi <aldenluthfi@gmail.com>
2024-05-01 21:35:58 +0700

    Updated susunluthfi

--- linux/scripts/stow_dotfiles.sh
+++
@@ -1,6 +0,0 @@
-start=$(pwd)
-
-cd $(dirname $0)/../home_directory
-sudo stow . --target=$HOME
-
-cd $start
--- linux/scripts/unstow_dotfiles.sh
+++
@@ -1,6 +0,0 @@
-start=$(pwd)
-
-cd $(dirname $0)/../home_directory
-sudo stow -D . --target=$HOME
-
-cd $start
