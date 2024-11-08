1b86e14a35b2befff982cabc9043456de0424e54
aldenluthfi <aldenluthfi@gmail.com>
2024-11-08 11:32:08 +0700

    add: packages

--- linux/scripts/install.sh
+++ linux/scripts/install.sh
@@ -3,7 +3,7 @@ cat pacman_packages | tr '\n' ' ' | xargs sudo pacman -Syu --noconfirm
 ./install_paru.sh
 ./clean_packages.sh
 
-cat paru_packages | tr '\n' ' ' | xargs paru --noconfim
+cat paru_packages | tr '\n' ' ' | xargs paru --noconfirm
 
 ./stow_dotfiles.sh
 ./sddm_config.sh
